6615251bb633e2f8946c5788ca281abd99ff73df
rustiandacode <rustiandazen09@gmail.com>
2023-05-16 01:26:29 +0700

    added homepage and show data

---
+++ clients/src/components/WorkoutDetails.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const WorkoutDetails = ({ workout }) => {
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>
+          Load (kg) : <strong>{workout.load}</strong>
+        </strong>
+      </p>
+      <p>
+        <strong>
+          Reps : <strong>{workout.reps}</strong>
+        </strong>
+      </p>
+      <p>{workout.created}</p>
+    </div>
+  );
+};
+
+export default WorkoutDetails;
